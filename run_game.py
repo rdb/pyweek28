49b221934e4ef581ee67559984ef37d35f67aa8e
rdb <git@rdb.name>
2019-09-28 22:55:30 +0200

    Show big warning if requirements are not installed

--- run_game.py
+++ run_game.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+try:
+    import panda3d.core
+except ImportError:
+    print("Cannot import panda3d.  Did you install the requirements?  Try running:")
+    print("")
+    print("  pip install -r requirements.txt")
+    print("")
+    print("Original exception was:")
+
 from game import main
 
 if __name__ == '__main__':
