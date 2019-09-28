6e7923c3367d2dd956e03b90fc3bbb610e401486
rdb <git@rdb.name>
2019-09-28 20:23:32 +0200

    Add clocky wocky timey wimey level, as the PyWeek clock ticks down

--- game/__init__.py
+++ game/__init__.py
@@ -10,7 +10,7 @@ from .input import Input
 
 ASPECT_RATIO = 16 / 9.0
 
-FLOORS = ['rusty', 'nature', 'light', 'bells']
+FLOORS = ['rusty', 'nature', 'time', 'light', 'bells']
 
 
 class Game(ShowBase):
