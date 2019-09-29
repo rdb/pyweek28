c489fa2dd812338a3103749ea04c99490cc10c6e
rdb <git@rdb.name>
2019-09-29 16:18:43 +0200

    Add setup.py for deployment

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='hobots-ascent',
+    version='1.0.0',
+    options={
+        'build_apps': {
+            'include_patterns': {
+                'assets/**',
+                'settings.prc',
+                'README.md',
+            },
+            'include_modules': {
+                'run_game': [
+                    'game.floors.title',
+                    'game.floors.rusty',
+                    'game.floors.nature',
+                    'game.floors.time',
+                    'game.floors.light',
+                    'game.floors.bells',
+                    'game.floors.end',
+                ],
+            },
+            'gui_apps': {
+                'run_game': 'run_game.py',
+            },
+            'log_filename': "$USER_APPDATA/Hobot's Ascent/output.log",
+            'log_append': False,
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+                'p3fmod_audio',
+            ],
+        },
+    }
+)
